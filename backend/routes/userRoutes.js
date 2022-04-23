145e2dda4a8f7d9262cbe2d3419959cf39c7a3ec
IT20606442 <it20606442@my.sliit.lk>
2022-04-24 00:16:38 +0530

    user route changed

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,4 +6,6 @@ const { protect } = require("../middlewares/authMiddleware");
 router.route("/").post(registerUser);
 router.route("/login").post(authUser);
 router.route("/profile").post(protect, updateUserProfile);
+
+
 module.exports = router;
